fb9bb22b616b25b4967e5b5dbdcfb7b8b0bce25c
Victoriia Miroshnikova <victoriia.pl@gmail.com>
2023-02-08 12:15:09 +0200

    Поправила 5 завдання

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,10 @@
-const form = {
-    input: document.querySelector("#name-input"),
-    text: document.querySelector("#name-output"),
-};
 
-const defaultValue = form.text.textContent;
-
-const onInputChange = (event) => {
-    
-    form.text.textContent = event.currentTarget.value;
-        if (form.text.textContent.length === 0) {
-            return form.text.textContent = defaultValue;
-        };
-};
-
-form.input.addEventListener("input", onInputChange);
+const input = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+function onTypingInInput(e) {
+    nameOutput.textContent = e.currentTarget.value.trim();
+        if (!nameOutput.textContent) {
+            nameOutput.textContent = "Anonymous";
+        }
+}
+input.addEventListener('input', onTypingInInput)
