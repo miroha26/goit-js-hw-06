6ab5196608bac35c841b7f7c142ddaa9a255c5e4
Victoriia Miroshnikova <victoriia.pl@gmail.com>
2023-02-08 12:10:39 +0200

    Поправила 5 завдання

--- js/task-05.js
+++ js/task-05.js
@@ -7,10 +7,10 @@ const defaultValue = form.text.textContent;
 
 const onInputChange = (event) => {
     
- form.text.textContent = event.currentTarget.value;
-  if (form.text.textContent.length === 0) {
-    return form.text.textContent = defaultValue;
- };
+    form.text.textContent = event.currentTarget.value;
+        if (form.text.textContent.length === 0) {
+            return form.text.textContent = defaultValue;
+        };
 };
 
 form.input.addEventListener("input", onInputChange);
